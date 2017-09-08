da0866cef96efa1fddcd8fa3c53907b744e90b45
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 20:01:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,8 +2,10 @@ var playlist = new Objest();
 
 function updatePlaylist(p, artist, song){
   p[artist] = song;
+  return p;
 }
 
 function removeFromPlaylist(p, artist){
   delete p[artist];
+  return p;
 }
